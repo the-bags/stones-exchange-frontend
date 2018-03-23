d062bd953dc60ddd5219a991e3df5cba880957dd
raymondproduction@ukr.net <raymondproduction@ukr.net>
2018-03-23 02:41:12 +0200

    Added quote with Ecclesiastes 3:5

---
+++ src/Quote.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="quote">
+<div class="card">
+  <div class="card-header">
+    Quote
+  </div>
+  <div class="card-body">
+    <blockquote class="blockquote mb-0">
+      <p>A time to cast away stones, and a time to gather stones together</p>
+      <footer class="blockquote-footer">Ecclesiastes  <cite title="Source Title">3:5</cite></footer>
+    </blockquote>
+  </div>
+</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'quote'
+}
+</script>
+
+<style>
+</style>
