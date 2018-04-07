a6e6cca6bb65500cf40ddaa8f8b5b5963746fb69
Bogdan Karlenko <opposite.ps@gmail.com>
2018-04-07 10:37:15 +0300

    STON: add userlist

--- src/main.js
+++ src/main.js
@@ -2,18 +2,23 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
-import Quote from './components/Quote'
+import VueResource from 'vue-resource'
 import router from './router'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import Quote from './components/Quote'
+import UserListPage from './components/UserListPage'
+
 Vue.use(BootstrapVue)
+Vue.use(VueResource)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  template: '<Quote/>',
-  components: { Quote }
+  template: '<UserListPage/>',
+  components: { Quote, UserListPage }
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
 
+import Hello from '@/components/Hello'
+import UserListPage from '../components/UserListPage'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/users',
+      name: 'UserList',
+      component: UserListPage
     }
   ]
 })
