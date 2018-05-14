d8a4e89e13026f0bafa26a25059badcbe6a837c6
raymondproduction@ukr.net <raymondproduction@ukr.net>
2018-05-14 20:39:47 +0300

    Mouse position

--- src/app/ui/components/space/space.component.ts
+++ src/app/ui/components/space/space.component.ts
@@ -17,27 +17,50 @@ export class SpaceComponent implements OnInit {
 
   title = 'Stone space';
   stones: IStone[];
+  canvas: HTMLCanvasElement;
+  ctx: CanvasRenderingContext2D;
 
   @ViewChild('canvas') canvasRef: ElementRef;
 
-  constructor(private stonesService: StonesService) { }
+  constructor(private stonesService: StonesService) {}
+
+  writeMessage(message) {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.font = '18pt Calibri';
+    this.ctx.fillStyle = 'black';
+    this.ctx.fillText(message, 10, 25);
+  }
+  getMousePos(evt) {
+    const rect = this.canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
+  }
 
   async ngOnInit() {
     this.stones = <[IStone]> await this.stonesService.getStones();
+    this.canvas = this.canvasRef.nativeElement;
+    this.ctx = this.canvas.getContext('2d');
     console.log(this.stones);
-    const ctx: CanvasRenderingContext2D =
-    this.canvasRef.nativeElement.getContext('2d');
-    ctx.fillStyle = 'black';
-    ctx.font = '20px Georgia';
-    ctx.fillText(this.title, 10, 20);
+
+    this.ctx.fillStyle = 'black';
+    this.ctx.font = '20px Georgia';
+    this.ctx.fillText(this.title, 10, 20);
     let y = 40;
     this.stones.forEach((stone: IStone) => {
-      ctx.beginPath();
-      ctx.arc(40, y += 20, 10, 0, 2 * Math.PI);
-      ctx.fillStyle = stone.color;
-      ctx.fill();
+      this.ctx.beginPath();
+      this.ctx.arc(40, y += 20, 10, 0, 2 * Math.PI);
+      this.ctx.fillStyle = stone.color;
+      this.ctx.fill();
     });
-    ctx.beginPath();
-}
+    this.ctx.beginPath();
+    const self = this;
 
+    this.canvas.addEventListener('mousemove', function(evt) {
+      const mousePos = self.getMousePos(evt);
+      const message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
+      self.writeMessage(message);
+    }, false);
+  }
 }
