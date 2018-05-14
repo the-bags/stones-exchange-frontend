cfaca85291495a699ae0e2320e93283d8f668580
raymondproduction@ukr.net <raymondproduction@ukr.net>
2018-05-15 00:23:56 +0300

    Clear space component for stone service

--- src/app/ui/components/space/space.component.ts
+++ src/app/ui/components/space/space.component.ts
@@ -24,20 +24,8 @@ export class SpaceComponent implements OnInit {
 
   @ViewChild('canvas') canvasRef: ElementRef;
 
-  constructor(private stonesService: StonesService) { }
+  constructor(public stonesService: StonesService) { }
 
-
-  getMousePos(evt) {
-    const rect = this.canvas.getBoundingClientRect();
-    return {
-      x: evt.clientX - rect.left,
-      y: evt.clientY - rect.top
-    };
-  }
-
-  clearStone(stone: IStone) {
-    this.ctx.putImageData(stone.background, stone.x - this.stoneRadius - 5, stone.y - this.stoneRadius - 5);
-  }
 
   async ngOnInit() {
     await this.stonesService.getStones();
@@ -46,32 +34,4 @@ export class SpaceComponent implements OnInit {
     this.stonesService.inventoryShow();
   }
 
-  stoneTake(event) {
-    if (this.stone) {
-      this.stone.drag = false;
-    }
-    const self = this;
-    this.stones.forEach((stone: IStone) => {
-      const mousePos = self.getMousePos(event);
-      if (Math.pow(mousePos.x - stone.x, 2) + Math.pow(mousePos.y - stone.y, 2) < Math.pow(self.stoneRadius, 2)) {
-        this.stone = stone;
-        this.stone.drag = true;
-      }
-    });
-  }
-  stoneDrop(event) {
-    if (!this.stone || !this.stone.drag) { return; }
-    this.clearStone(this.stone);
-    this.stone.x = this.getMousePos(event).x;
-    this.stone.y = this.getMousePos(event).y;
-    this.putStone(this.stone);
-    this.stone.drag = false;
-  }
-  stoneDrag(event){
-    if (!this.stone || !this.stone.drag) { return; }
-    this.clearStone(this.stone);
-    this.stone.x = this.getMousePos(event).x;
-    this.stone.y = this.getMousePos(event).y;
-    this.putStone(this.stone);
-  }
 }
